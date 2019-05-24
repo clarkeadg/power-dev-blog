1890abea0d283894f796063cf577c842802bf319
Brian Clarke <clarkeadg@yahoo.com>
2019-05-24 00:48:19 -0700

    sass

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,25 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { BlogPostTemplate } from "../../templates/post.tsx";
-import CSSInjector from "../../components/CSSInjector.js";
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
   const data = entry.getIn(['data']).toJS()
   console.log("BlogPostPreview", data)
   
   return (
-    <CSSInjector>
-      <BlogPostTemplate
-        content={widgetFor('body')}
-        description={entry.getIn(['data', 'description'])}
-        tags={entry.getIn(['data', 'tags'])}
-        title={entry.getIn(['data', 'title'])}
-        image={entry.getIn(['data', 'image'])}
-        author={entry.getIn(['data', 'author'])}
-        date={entry.getIn(['data', 'date'])}
-        layout={entry.getIn(['data', 'layout'])}
-      />
-    </CSSInjector>
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={entry.getIn(['data', 'tags'])}
+      title={entry.getIn(['data', 'title'])}
+      image={entry.getIn(['data', 'image'])}
+      author={entry.getIn(['data', 'author'])}
+      date={entry.getIn(['data', 'date'])}
+      layout={entry.getIn(['data', 'layout'])}
+    />
    ) 
 }
 
